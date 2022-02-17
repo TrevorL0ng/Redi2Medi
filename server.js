c07e0edc99bc72603474813ee93a8cf5777e1a3a
Trevor L <trevorlong992@gmail.com>
2022-02-17 17:17:50 -0500

    Working on the database functionality pt12 I have deeply delved

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var moment = require('moment');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+//   cookie: {},
   resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
+  saveUninitialized: true
+//   store: new SequelizeStore({
+//     db: sequelize,
+//   }),
 };
 
 app.use(session(sess));
