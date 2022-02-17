52c2abbf1efbe238f289460d903b309b15d917e0
Trevor L <trevorlong992@gmail.com>
2022-02-17 17:22:33 -0500

    Working on the database functionality pt14 Brain hurts

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var moment = require('moment');
 const helpers = require('./utils/helpers');
+const routes = require('./controllers');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
